f7dcb9938339505ce7d194ce63562d3e37876319
ted <106636585+tarunsairaj@users.noreply.github.com>
2024-03-10 23:32:04 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
-  cout << "Webhook enabled successfully \n";
+  cout << "Webhook enabled successfully :) \n";
   return 0;
 }
